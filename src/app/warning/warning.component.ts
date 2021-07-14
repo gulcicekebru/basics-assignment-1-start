4d2bc4a31a84faf1f0b9a4f433903474ea66d7e5
Ebru Gülçiçek <gulcicekebru1@gmail.com>
2021-07-14 20:07:01 +0300

    Initial commit

---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component ({
+    selector:'app-warning',
+    template:'<h3>WARNING!</h3>',
+    styles:[`
+        h3 {
+            color:red;
+        }
+    `]
+    
+
+})
+
+export class WarningAlert {
+
+}
